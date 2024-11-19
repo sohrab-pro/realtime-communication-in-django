935548d443bea51c53c6ec618b56c71ee42d3b9c
Sohrab Khan <122888702+sohrabmvsol@users.noreply.github.com>
2024-11-19 18:37:19 +0500

    online dot for online users added

--- a_rtchat/routing.py
+++ a_rtchat/routing.py
@@ -2,6 +2,7 @@ from django.urls import path, re_path
 from .consumers import *
 
 websocket_urlpatterns = [
-    path("ws/chatroom/<chatroom_name>/", ChatroomConsumer.as_asgi()),
     path("ws/test/", TestConsumer.as_asgi()),
+    path("ws/chatroom/<chatroom_name>/", ChatroomConsumer.as_asgi()),
+    path("ws/online-status/", OnlineStatusConsumer.as_asgi()),
 ]
